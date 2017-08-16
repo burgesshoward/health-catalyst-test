845febd1598d3c111ffdf0d61e6ba94231d411c9
Burgess Howard <30877040+burgesshoward@users.noreply.github.com>
2017-08-16 16:41:24 -0600

    Adding my initial application

---
+++ PeopleApp/Models/DBinitialize.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace PeopleApp.Models
+{
+    public static class DBinitialize
+    {
+        public static void EnsureCreated(IServiceProvider serviceProvider)
+        {
+            var context = new PeopleContext(serviceProvider.GetRequiredService<DbContextOptions<PeopleContext>>());
+
+            context.Database.EnsureCreated();
+        }
+    }
+}
---
+++ PeopleApp/Models/PeopleContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PeopleApp.Models
+{
+    public class PeopleContext : DbContext
+    {
+        public PeopleContext(DbContextOptions<PeopleContext> options) : base(options)
+        {
+        }
+
+        public DbSet<PeopleApp.Models.Person> Person { get; set; }
+    }
+}
